fc73a819d2786a44907912674920d4099babfbc3
Alistair Chambers <38084528+Cambrian34@users.noreply.github.com>
2022-03-29 11:23:24 -0400

    Completed assignment 6

--- Alistair_Chambers_Program_6.asm
+++ Alistair_Chambers_Program_6.asm
@@ -1,100 +1,75 @@
-## CS 254 Program 5
-##
-## Compute (8xˆ2-3x+12) / (3x2+2x-16)
+## CS 254 Program 6
+##computes the sum of the odd integers and the sum of even integers from 0 to N.
 ##
 ## Programmer: Alistair Chambers
-## Date: 03/13/22
+## Date: 03/27/22
 ##Register use:
-##          r7= base register
-##          r8=x
-##          r9=8xˆ2,8xˆ2-3x,8xˆ2-3x+12
-##          r10=3,3x
-##          r11=12
-##          r12=2,2x
-##          r13=16
-##          r14=3,3xˆ2,3xˆ2+2x,(3xˆ2+2x-16)
-##          r15=xˆ2
-##          r16=0
-##          r17=1
-##          r24=quotient
-##          r24=remainder
+##          r7= n
+##          r8=count
+##          r9=scratch
+##          r10=base register
+##          r11=2, value used to divide n by
+##          r12=floor
+##          r13=remainder
+##          r14= evensum += count
+##          r15= oddsum += count
+##          r16=0 used to check if count number is even
+##
 
 
     .text
     .globl  main
 
 
+    init:
+        lui       $10,0x1000           #base Register
+        lw        $7, 0($10)           # loads n from data
+        ori       $8,$0,0              #counter
+        ori       $11,$0,2             #used to divide n by to check if it odd or even
+        ori       $14,$0,0             #stores even sum after each increment
+        ori       $15,$0,0             #stores odd sum after each increment
+        ori       $16,$0,0             #used to check if even or odd
+    test:
+        sltu      $9,$8,$7             #count <n
+        beq       $9,$0,endLp          # if $9== n then jump to endLp
+        sll       $0,$0,0              #nop
 
-    main:
-            lui     $7,0x1000         #Init base register
-            lw      $8,0($7)          #loads x
+        addiu     $8,$8,1              # count increments;
 
-            ori     $9,$0,8           #assigns 18 to register 9
-            ori     $10,$0,3          #assigns 18 to register 10
-            ori     $11,$0,12         #assigns 18 to register 11
-            ori     $14,$0,3          #assigns 18 to register 14
-            ori     $12,$0,2          #assigns 18 to register 12
-            ori     $13,$0,16         #assigns 18 to register 13
+        div       $8, $11              # $8 /$11
+        mflo      $12                  # $12= floor($8 /$12 )
+        mfhi      $13                  # $13 =  mod
 
+        beq       $13, $16,even       # if  remainder == 0 ,even, then jump to even
+        sll       $0,$0,0              #nop
+        bne       $13, $16,odd        # if  remainder != 0 ,odd,  then jump to odd
+        sll       $0,$0,0              #nop
 
-            mult    $8, $8            #xˆ2
-            mflo    $15               #$15= xˆ2
+        j         endLp                # jump to test
+        sll       $0,$0,0              #nop
 
-            mult    $15, $9           # 8xˆ2
-            mflo    $9                #$9=8xˆ2
+    even:
+        addu      $14,$14,$8          # evensum += count
+        j         test                # jump to test
+        sll       $0,$0,0             #nop
 
-            mult    $10, $8           # 3x
-            mflo    $10               #$10=3x
+    odd:
+        addu      $15,$15,$8          # oddsum += count
+        j         test                # jump to test
+        sll       $0,$0,0             #nop
 
-
-            sub     $9, $9, $10       #$9=8xˆ2-3x
-            add     $9, $9, $11       #$9=8xˆ2-3x+12
-
-            mult    $14, $15          #3xˆ2
-            mflo    $14               # $14=3xˆ2
-            sll     $0,$0,0           #nop
-
-            mult    $12, $8           #$12=2x
-            mflo    $12               # $12=2x
-            sll     $0,$0,0           #nop
-
-            add     $14, $14, $12     # $14 = 3xˆ2+2x
-            sub     $14, $14, $13     # $14 = (3xˆ2+2x-16)
-
-            ori     $16,$0,0          #$16=0
-
-            bne     $14, $16,cont     # if  denominator != 0 then branch to cont
-            sll     $0,$0,0           #nop
-            ori     $17,$0,1          #$17=1
-
-            sw      $17, 4($7)        # Stores 1 in data error
-            j       end               # jump to end
+    endLp:
+        sw        $14,4($10)          #Stores $14 in data under evensum
+        sw        $15,8($10)          #Stores $15 in data under oddsum
+        sll       $0,$0,0             #nop
 
 
 
+                 .data
+        N:       .word   12
 
-    cont:
-            sw      $16, 4($7)        # Stores 0 in data error
-            divu    $9, $14           # (8xˆ2-3x+12) / (3x2+2x-16)
-            mflo    $24               # $24=quotient
-            mfhi    $25               # $25=remainder
-            sll     $0,$0,0           #nop
+        evensum: .word   0
 
-            sw      $24, 8($7)        #stores quotient in ratio
+        oddsum:  .word   0
 
-            sw      $25, 16($7)       #stores remainder in remain
-            j       end               # jump to end
-    end:
-            sll     $0,$0,0           #nop
-
-
-            .data
-
-x:          .word    -13
-
-error:      .word    1
-
-ratio:      .word    0
-
-remain:     .word    0
 ##end of file
---
+++ Alistair_Chambers_Program_7.asm
@@ -0,0 +1,31 @@
+## CS 254 Program 6
+##computes the sum of the odd integers and the sum of even integers from 0 to N.
+##
+## Programmer: Alistair Chambers
+## Date: 04/03/22
+##Register use:
+##          r7= n
+##          r8=count
+##          r9=scratch
+##          r10=base register
+##          r11=2, value used to divide n by
+##          r12=floor
+##          r13=remainder
+##          r14= evensum += count
+##          r15= oddsum += count
+##          r16=0 used to check if count number is even
+##
+
+
+    .text
+    .globl  main
+
+        main:
+
+
+
+                 .data
+
+
+        
+##end of file
