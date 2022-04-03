76c9b1a1257816abf47123411f3de760d961f77e
Alistair Chambers <38084528+Cambrian34@users.noreply.github.com>
2022-04-03 17:40:52 -0400

    a linear congruence generator(Program 7)

--- Alistair_Chambers_Program_7.asm
+++ Alistair_Chambers_Program_7.asm
@@ -1,5 +1,6 @@
-## CS 254 Program 6
-##computes the sum of the odd integers and the sum of even integers from 0 to N.
+## CS 254 Program 7
+##
+##Enter seed and how many random ints are desired.
 ##
 ## Programmer: Alistair Chambers
 ## Date: 04/03/22
@@ -15,17 +16,97 @@
 ##          r15= oddsum += count
 ##          r16=0 used to check if count number is even
 ##
-
-
     .text
     .globl  main
 
-        main:
+
+    init:
+        li        $v0,4                #  =
+        la        $a0, prompt          #
+        syscall
+
+        li        $v0,  5        #$v0 = 5
+        syscall
+        move      $7, $v0        #7=
+
+        li        $v0,4                #  =
+        la        $a0, how          #
+        syscall
+
+        li        $v0,  5        #$v0 = 5
+        syscall
+        move      initial, $v0        #7=
+
+#redacted #lw        $7,inputi            # loads input from data
+        lw        $12,a                # a
+        lw        $13,bb               #bb
+
+        ori       $8,$0,0              #counter
+        ori       $24,$0,100
+
+    test:
+        lw        $11,initial          # initial value
+
+        sltu      $9,$8,$7             #count <n
+        beq       $9,$0,endLp          # if $9== input n then jump to endLp
+
+        sll       $0,$0,0              #nop
+
+        addiu     $8,$8,1              # count increments;
+        sll       $0,$0,0              #nop
+
+        j         rand                # jump to test
+
+    rand:
+
+        mult      $11, $12               #  *  = Hi and Lo registers
+        mflo      $14                    # copy Lo to
+
+        addu      $14, $14, $13            #  =  +
+
+        div       $14, $24            #  /
+        mfhi      $25                   #  =  mod
+        sw        $25, initial        #
+
+        sw        $25, out        #
+
+        li        $v0, 4         #$v0  4=
+
+        li        $v0,1                #  =
+        la        $a0, out          #
+        syscall
 
 
 
-                 .data
+        j         test                # jump to test
 
 
-        
+    endLp:
+        li        $v0,4                #  =
+        la        $a0, done          #
+        sll       $0,$0,0              #nop
+        syscall
+
+
+                  .data
+
+    prompt:       .asciiz     "initial value : "
+
+    done:         .asciiz     "Done"
+
+    how:          .asciiz     "Enter how many: "
+
+
+
+    inputi:        .word      20
+
+    initial:      .word       5
+
+    a:            .word       21
+
+    bb:           .word       53
+
+    out:          .word       10
+
+
 ##end of file
